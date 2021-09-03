94d7b020d56c4066a567899b4d6129ac5a9731da
Matija Fućek <matijafucek1@gmail.com>
2021-09-03 17:16:39 +0200

    loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/global.css';
 import '../styles/grid.css';
 
+import Router from 'next/router';
+
+import NProgress from 'nprogress'; //nprogress module
+import '../styles/nprogress.css'; //styles of nprogress
+
 import type { AppProps } from 'next/app';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
